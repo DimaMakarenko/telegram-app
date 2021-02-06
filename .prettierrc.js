7109cbecad4bce0653c9390afcc5318ba66d2d21
DimaMakarenko <makarenko.d.viri@gmail.com>
2021-02-06 17:03:14 +0200

    init of project structure

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  arrowParens: 'always',
+  trailingComma: 'all',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  endOfLine: 'auto',
+};
